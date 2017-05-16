bab312d7eb8dd5eee84cd4259a1be61a102fc118
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-05-16 17:29:35 -0400

    create and add functions working

--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+//empty arry to push new households
+var householdArray = [];
+
+//create household object and form reset
+function householdPerson(age, relationship, smoker){
+  var element = document.createElement("li");
+  var person = age + ", " + relationship + ", ";
+  person += (smoker) ? "smoker" : "non-smoker";
+  element.appendChild(document.createTextNode(person));
+  var data = document.getElementsByTagName("ol")[0];
+  data.appendChild(element);
+  var form = document.getElementsByTagName("form")[0];
+  form.reset();
+    return {
+     age: age,
+     relationship: relationship,
+     smoker: smoker
+   };
+}
+
+//add button that gets input from form and pushes it into the array
+var addButton = document.getElementsByClassName("add")[0];
+addButton.addEventListener("click", function(event){
+  var age = document.getElementsByName("age")[0].value;
+  var relationship = document.getElementsByName("rel")[0].value;
+  var smoker = document.getElementsByName("smoker")[0].checked;
+  householdArray.push(householdPerson(age, relationship, smoker));
+  event.preventDefault();
+});
